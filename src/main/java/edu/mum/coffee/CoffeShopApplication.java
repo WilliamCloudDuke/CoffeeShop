7958eee8225373591f2d2383fa59f2101e6f2463
William Chaparro <31998829+Williamcitor@users.noreply.github.com>
2018-06-07 11:51:08 -0500

    Undo the test

--- src/main/java/edu/mum/coffee/CoffeShopApplication.java
+++ src/main/java/edu/mum/coffee/CoffeShopApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoffeShopApplication {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test");
 		SpringApplication.run(CoffeShopApplication.class, args);
 	}    
 }
